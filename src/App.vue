ce59ff6686c3efb44389b2a38d7ffa8b6041ed8e
Line <line@datahjelpen.no>
2020-01-03 15:21:24 +0100

    Add Vue app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <InfoWrapper />
+  </div>
+</template>
+
+<script>
+import InfoWrapper from './components/InfoWrapper.vue'
+
+export default {
+  name: 'app',
+  components: {
+    InfoWrapper
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
