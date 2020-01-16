93bb8ef28c724e6f61926d762090834fcfb9c107
Line <line@datahjelpen.no>
2020-01-16 10:57:01 +0100

    Add styling

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,7 @@ export default {
     display: flex;
     justify-content: center;
     /* background-color: linear-gradient(to right, #191948, #2F2F82); */
-      background-image: linear-gradient(to bottom right, #2F2F82, #191948);
-
+      background-image: linear-gradient(to right, #fa709a 0%, #fee140 100%);
   }
 } 
 
--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="info">
-        <NavBar />
         <h1>{{ fullname }}</h1>
+        <NavBar />
     </div>
 </template>
 
@@ -22,11 +22,15 @@ export default {
 <style>
     h1 {
         font-size: 6em;
-        width: 100%;
+        width: 90%;
+        margin: 0 auto;
         font-family: 'Playfair Display', serif;
     }
     
     .info {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
     }
 
     @media screen and (min-width: 1024px) {
