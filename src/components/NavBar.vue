c75050934c88b58e49845a0b27b349c0addd0964
Line <line@datahjelpen.no>
2020-02-06 15:33:53 +0100

    Add link to home page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,21 @@
 <template>
     <nav>
+        <span v-if="!main"><router-link to="/">Hjem</router-link></span>
         <router-link to="/about">Om meg</router-link>
         <router-link to="/references">Referanser</router-link>
         <router-link to="/contact">Kontakt</router-link>
     </nav>
 </template>
+
+<script>
+export default {
+    computed: {
+        main() {
+            return this.$route.path === '/'
+        }
+    }
+}
+</script>
 
 <style>
     nav {
